1cd6ff4a7aeaa56a2302a65b36cc0dd1b9cbf1ac
Kenta Urakami <ken.atnek@gmail.com>
2025-07-17 15:24:42 +0900

    🧑‍💻 初期設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,171 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+/* =======================================
+ * 熊本市中央公民館 TOPページ
+ * URL: /app/page.tsx
+ * Created: 2025-07-11
+ * Last updated: 2025-07-11
+ * ======================================= */
 
+import type { Metadata } from 'next';
+import styles from '@/styles/PageTop.module.scss';
+import Image from 'next/image';
+import ExternalLink from '@/components/common/ExternalLink';
+import BlockFacilityGuide from '@/components/BlockFacilityGuide';
+import BlockFacilityDetails from '@/components/BlockFacilityDetails';
+import BlockFacilityHours from '@/components/BlockFacilityHours';
+import BlockCourseGuide from '@/components/BlockCourseGuide';
+import BlockAccess from '@/components/BlockAccess';
+import BlockStore from '@/components/BlockStore';
+import { newsData } from '@/data/newsData';
+import BlockNewsList from '@/components/BlockNewsList';
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: '熊本市中央公民館',
+    description:
+      '熊本市中央公民館は図書館から会議室、ホールまで幅広くご利用いただける施設です。',
+  };
+};
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <>
+      <section className={styles.containerHero}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/images/hero.webp"
+          alt="人々が集い 学ぶ 災害に強いまちづくりの拠点"
+          width={100}
+          height={100}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      </section>
+      <section className={styles.containerNews} id="ContainerNews">
+        <article>
+          <h2 className={styles.itemH2}>お知らせ</h2>
+          <p className={styles.sidebarH2}>
+            講座案内や公民館だよりについてなど最新情報をお届けします
+          </p>
+          <ExternalLink
+            href="/data/pdf/spring202502.pdf"
+            aria-label="（白川）カルチャー講座　春期講座　受講生募集中！"
+            className={styles.itemHeadPdf}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            （白川）カルチャー講座　春期講座　受講生募集中！
+          </ExternalLink>
+          <BlockNewsList items={newsData} />
+        </article>
+      </section>
+      <section
+        className={styles.containerFacilityGuide}
+        id="ContainerFacilityGuide"
+      >
+        <div className={styles.boxHead}>施設ご利用に関するトピックス</div>
+        <article>
+          <h2 className={styles.itemH2}>施設のご案内</h2>
+          <p className={styles.sidebarH2}>
+            画像をクリックすると詳しい情報が見れます
+          </p>
+          <BlockFacilityGuide />
+        </article>
+      </section>
+      <section
+        className={styles.containerFacilityDetails}
+        id="ContainerFacilityDetails"
+      >
+        <article>
+          <h2 className={styles.itemH2}>施設ご利用について</h2>
+          <p className={styles.sidebarH2}>利用お申し込み、料金、定員について</p>
+          <BlockFacilityDetails />
+        </article>
+      </section>
+      <section
+        className={styles.containerFacilityHours}
+        id="ContainerFacilityHours"
+      >
+        <article>
+          <h2 className={styles.itemH2}>施設利用可能時間</h2>
+          <p className={styles.sidebarH2}>利用時間</p>
+          <BlockFacilityHours />
+        </article>
+      </section>
+      <section
+        className={styles.containerCourseGuide}
+        id="ContainerCourseGuide"
+      >
+        <article>
+          <h2 className={styles.itemH2}>講座のご案内</h2>
+          <BlockCourseGuide />
+        </article>
+      </section>
+      <section className={styles.containerAccess} id="ContainerAccess">
+        <article>
+          <h2 className={styles.itemH2}>アクセス</h2>
+          <BlockAccess />
+        </article>
+      </section>
+      <section className={styles.containerTearoom} id="ContainerTearoom">
+        <article>
+          <h2 className={styles.itemH2}>白川公園茶室について</h2>
+          <dl>
+            <dt>申し込み開始日</dt>
+            <dd>前年度の10月より開始</dd>
+          </dl>
+          <dl>
+            <dt>料金のお支払い</dt>
+            <dd>
+              利用料は前納です。申込の際にお支払いください。
+              <br />
+              茶道具の一部は、無料で貸し出します。
+            </dd>
+          </dl>
+          <dl>
+            <dt>連絡先</dt>
+            <dd>
+              <div>
+                白川公園茶室事務所
+                <ExternalLink
+                  href="tel:0963545280"
+                  aria-label="096-354-5280に電話"
+                  className={styles.itemTel}
+                >
+                  096-354-5280
+                </ExternalLink>
+              </div>
+            </dd>
+          </dl>
+          <dl>
+            <dt>お申し込み/予約変更方法</dt>
+            <dd>
+              <p>
+                ご来館、またはファクスで承ります。以下書類をダウンロードして必要事項をご記入ください。
+              </p>
+              <nav>
+                <ExternalLink
+                  href="../../public/data/pdf/白川公園茶室予約申込書202209.pdf"
+                  aria-label="予約申込書PDF"
+                >
+                  予約申込書PDF
+                </ExternalLink>
+                <ExternalLink
+                  href="../../public/data/pdf/shirakawahenkou.pdf"
+                  aria-label="予約変更依頼書PDF"
+                >
+                  予約変更依頼書PDF
+                </ExternalLink>
+                <ExternalLink
+                  href="../../public/data/pdf/2024優先予約申込書.pdf"
+                  aria-label="優先予約申込書（100人以上の催事）PDF"
+                >
+                  優先予約申込書（100人以上の催事）PDF
+                </ExternalLink>
+              </nav>
+            </dd>
+          </dl>
+        </article>
+      </section>
+      <section className={styles.containerStore} id="ContainerStore">
+        <article>
+          <h2 className={styles.itemH2}>喫茶・売店 しらかわのほとり</h2>
+          <BlockStore />
+        </article>
+      </section>
+    </>
   );
 }
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,132 @@
+/* =======================================
+ * 熊本市中央公民館 HEADER
+ * URL: src/components/common/Header.tsx
+ * Created: 2025-07-11
+ * Last updated: 2025-07-11
+ * ======================================= */
+'use client';
+import styles from '@/styles/components/common/Header.module.scss';
+import { useEffect, useState, useRef } from 'react';
+import Link from 'next/link';
+import ExternalLink from '@/components/common/ExternalLink';
+import Image from 'next/image';
+
+import Logo from '@/assets/images/logo.webp';
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        navRef.current &&
+        !navRef.current.contains(event.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        closeMenu();
+      }
+    };
+    document.addEventListener('click', handleOutsideClick, true);
+    return () =>
+      document.removeEventListener('click', handleOutsideClick, true);
+  }, [isOpen]);
+
+  return (
+    <header className={styles.containerHeader}>
+      <article>
+        <h1>
+          <Link href="/" className={styles.linkTop}>
+            <Image
+              src={Logo}
+              alt="熊本市中央公民館のロゴ"
+              width={250}
+              height={36}
+            />
+          </Link>
+        </h1>
+        <nav
+          ref={navRef}
+          className={`${styles.wrapMobileMenu} ${isOpen ? styles['is-open'] : ''} ${
+            !isOpen ? styles.closing : ''
+          }`}
+        >
+          <Link
+            href="#ContainerNews"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
+            お知らせ
+          </Link>
+          <Link
+            href="#ContainerFacilityGuide"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
+            施設のご案内
+          </Link>
+          <Link
+            href="#ContainerCourseGuide"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
+            講座のご案内
+          </Link>
+          <Link
+            href="#ContainerFacilityDetails"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
+            利用申込・利用料金
+          </Link>
+          <Link
+            href="#ContainerAccess"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
+            アクセス
+          </Link>
+          <ExternalLink
+            href="https://zform.kumanichi.com/td0625g/form/Untitled32/formperma/4qWnLqGcH38dthvgOBJv34Mpt-viegFsAdIs-s-fnPg"
+            aria-label="熊本市中央公民館へのお問い合わせ"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
+            お問い合わせ
+          </ExternalLink>
+        </nav>
+      </article>
+      <button
+        type="button"
+        className={`${styles.hamburgerButton} ${
+          isOpen ? styles['is-open'] : ''
+        }`}
+        onClick={toggleMenu}
+        aria-expanded={isOpen}
+        aria-label="メニューを開閉"
+        ref={buttonRef}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </header>
+  );
+};
+
+export default Header;
